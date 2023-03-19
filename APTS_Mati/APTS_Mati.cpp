f8b9583674928b267bfccfc90f4f65d63ec19c22
clavinsh <klavins.ar@gmail.com>
2023-03-19 19:42:02 +0200

    Priority Queue implementation

--- APTS_Mati/APTS_Mati.cpp
+++ APTS_Mati/APTS_Mati.cpp
@@ -1,33 +1,100 @@
 #include <iostream>
 #include<fstream>
 
-int main()
-{
-    std::fstream File("hair.in");
+struct Barber {
+    unsigned int id;
+    unsigned int lastServiceTime;
 
-    if (!File.is_open()) {
-        return -1;
+    Barber(unsigned int id) {
+        this->id = id;
+        this->lastServiceTime = 0;
     }
-    unsigned int barbers = 0;
 
-    barbers << File.get();
+    // comparison function
+    // priority takes the one barber with the longest wait time
+    // if barbers have equal wait time, then priority takes the one with
+    // the smallest id
+    bool operator<(const Barber& other) const {
+        if (lastServiceTime != other.lastServiceTime) {
+            return lastServiceTime > other.lastServiceTime;
+        }
+        else {
+            return id > other.id;
+        }
+    }
+};
 
-    std::cout << barbers;
+struct Client {
+    unsigned int id;
+    unsigned int arrivalTime;
+    unsigned int serviceLength;
+
+    Client(unsigned int id, unsigned int arrivalTime, unsigned int serviceLength) {
+        this->id = id;
+        this->arrivalTime = arrivalTime;
+        this->serviceLength = serviceLength;
+    }
+};
+
+// priority queue implementation for barbers using a heap
+
+struct Node {
+    Barber data;
+    Node* next;
+
+    Node(const Barber& b) : data(b), next(nullptr) {}
+};
+
+class PriorityQueue {
+public:
+    // Add a Barber to the priority queue
+    void push(const Barber& b) {
+        Node* newNode = new Node(b);
+        if (head == nullptr || b < head-> data) {
+            newNode->next = head;
+            head = newNode;
+        }
+        else {
+            Node* curr = head;
+            while (curr->next != nullptr && curr->next->data < b) {
+                curr = curr->next;
+            }
+            newNode->next = curr->next;
+            curr->next = newNode;
+        }
+    }
+
+    // Remove and return the highest priority Barber from the queue
+    Barber pop() {
+        Barber result = head->data;
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        return result;
+    }
+
+    // Check if the priority queue is empty
+    bool empty() const {
+        return head == nullptr;
+    }
+
+    // Get the size of the priority queue
+    int size() const {
+        int count = 0;
+        Node* curr = head;
+        while (curr != nullptr) {
+            count++;
+            curr = curr->next;
+        }
+        return count;
+    }
+
+private:
+    Node* head = nullptr;
+};
 
 
-
-    //char c;
-    //while (File.get(c))
-    //    std::cout << c;
-    // 
-    // 
-    // 
-    //std::cout << "Hello World!\n";
-
-    File.close();
-
-
+int main() {
 
     return 0;
 }
-
