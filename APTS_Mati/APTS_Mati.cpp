58c32dfadeda12eaee5ffd05e06017e55cd61f59
clavinsh <klavins.ar@gmail.com>
2023-03-19 17:29:45 +0200

    gitignore changes

--- APTS_Mati/APTS_Mati.cpp
+++ APTS_Mati/APTS_Mati.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include<fstream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::fstream File("hair.in");
+
+    if (!File.is_open()) {
+        return -1;
+    }
+    unsigned int barbers = 0;
+
+    barbers << File.get();
+
+    std::cout << barbers;
+
+
+
+    //char c;
+    //while (File.get(c))
+    //    std::cout << c;
+    // 
+    // 
+    // 
+    //std::cout << "Hello World!\n";
+
+    File.close();
+
+
+
     return 0;
 }
+
